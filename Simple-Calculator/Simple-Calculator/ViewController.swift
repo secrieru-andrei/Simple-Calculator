43ac55c163f796301ebbc63a6a2c161fad306ea5
Secrieru Andrei <secrieru.andrei@yahoo.com>
2022-09-08 11:37:48 +0300

    Starter project

---
+++ Simple-Calculator/Simple-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple-Calculator
+//
+//  Created by Secrieru Andrei on 08.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
